c369ac3979f585075d53b7e57040f671c0a8e5fe
Pierce - Jones, Richard: BAGL South Africa <Richard.Pierce-Jones@barclayscapital.com>
2018-12-08 15:50:56 +0200

    Removed obsolete cascading update test.

--- example/src/components/Header.js
+++ example/src/components/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
 
     const { updateNewTodoText, addTodo } = useReduxBindActionCreators(headerActions);
     const newTodoText = useReduxState(state => state.newTodoText);
-    const todos = useReduxState(state => state.todos);
     const textChangeHandler = useCallback(e => updateNewTodoText(e.target.value), [updateNewTodoText]);
     const keypressHandler = useKeypressHandler({
         'Enter': addTodo,
@@ -16,7 +15,7 @@ const Header = () => {
 
     return (
             <header>
-                <h1>{todos.length} - todos</h1>
+                <h1>todos</h1>
                 <input type="text" placeholder="What needs to be done?" className="new-todo" value={newTodoText} onChange={ textChangeHandler } onKeyPress={ keypressHandler } />
             </header>
     );
